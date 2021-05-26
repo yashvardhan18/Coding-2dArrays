04a6decb8233de9163e240d0c9980547fc91c960
Yashvardhan <yashvardhan.gla_cs18@gla.ac.in>
2021-05-26 10:42:01 +0530

    q4 commited

---
+++ src/ExitPointOfAMatrix.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class ExitPointOfAMatrix {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+    }
+
+}
