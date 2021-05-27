bb97ac49012b1744574697e955bc6d51d2eae797
Yashvardhan <yashvardhan.gla_cs18@gla.ac.in>
2021-05-27 13:22:23 +0530

    q5 committed

---
+++ src/RotateBy90Degree.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void RotateBy90Degree(String[] args) throws Exception {
+        // write your code here
+
+    }
+
+    public static void display(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[0].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
