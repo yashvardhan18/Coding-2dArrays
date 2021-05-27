ad1e12fb41d75d5bc104f8fe8d8772897f9ac05b
Yashvardhan <yashvardhan.gla_cs18@gla.ac.in>
2021-05-27 14:51:25 +0530

    q5

--- src/RotateBy90Degree.java
+++ src/RotateBy90Degree.java
@@ -1,10 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class RotateBy90Degree {
 
-    public static void RotateBy90Degree(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         // write your code here
+        Scanner scn= new Scanner(System.in);
+        int n=scn.nextInt();
+        int m=scn.nextInt();
+        int [][] arr=new int[n][m];
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]=scn.nextInt();
+            }
+        }
+        for(int i=0;i< arr.length;i++){
+            for(int j=i;j<arr[0].length;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        for(int i=0;i< arr.length;i++){
+            int li=0;
+            int ri=arr[i].length-1;
+            while(li<ri){
+                int temp=arr[i][li];
+                arr[i][li]=arr[i][ri];
+                arr[i][ri]=temp;
+                li++;
+                ri--;
+            }
+        }
+        display(arr);
 
     }
 
@@ -16,5 +45,6 @@ public class Main {
             System.out.println();
         }
     }
+
 
 }
